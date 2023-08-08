c5acb646fa3e9985fc67f8540c08935781ea88f7
TkNazarii <tknazkooo@gmail.com>
2023-08-08 14:17:01 +0300

    first

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.subscribe(express.static("public"))
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/auth', authRouter)
@@ -26,7 +27,7 @@ res.status(404).json({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-	const { status = 500, message = 'Server errorrrrrrrrrr' } = err;
+	const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message })
 })
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {validateBody, authenticate} = require('../../middleware/index')
+const {validateBody, authenticate, upload} = require('../../middleware/index')
 const {schemas} = require('../../models/users') 
 
 const ctrl = require('../../controllers/auth')
@@ -13,6 +13,8 @@ router.post('/login', validateBody(schemas.loginSchema), ctrl.login)
 router.get("/current", authenticate, ctrl.getCurrent)
 
 router.post("/logout", authenticate, ctrl.logout)
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
 
 module.exports = router
 
